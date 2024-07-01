21a0a228f9851a545903e4b9f3313a665051d3cf
Francisco Arriaga <fran.arp@hotmail.com>
2024-06-30 21:49:17 -0600

    pre-loaded knn

--- retrieval_system.py
+++ retrieval_system.py
@@ -8,6 +8,7 @@ from model_loader import load_model
 from embedder import get_embeddings, load_precomp_embeddings
 from knn_retriever import get_knn_for_query
 
+# Load deep learning feature extractor
 visual_model = load_model()
 
 # Load and embed query image
@@ -17,4 +18,10 @@ query_embedding = get_embeddings(visual_model, [23], query_image)
 # Load candidate embeddings along with their filenames
 db_embeddings, db_filenames = load_precomp_embeddings(Path('./dataset/embeddings/23/'), names=True)
 
-knn_images, knn_E, distances = get_knn_for_query(query_embedding, knnbr, train_images, train_E)
+# Load pre-fit knn model
+
+# Retrieve query results from knn model
+knn_images, knn_E, distances = get_knn_for_query(query_embedding, knnbr, db_filenames, db_embeddings)
+
+# Function to display query results (not to be deployed, only for dev testing purposes)
+
